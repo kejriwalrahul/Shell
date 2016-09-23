aa69f05480e3a9e99b52522d3965da93df7a8ff1
krahul <krahul@aarunya.cse.iitm.ac.in>
2016-09-23 10:57:04 +0530

    init commit

---
+++ shell.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<string.h>
+
+const int size = 512;
+
+struct command{
+	char *cmd;
+	char **args;
+};
+
+char *builtInCmds[] = {"exit"}
+int builtInCmdSize = 1;
+
+int iswspace(char c){
+	switch(c){
+		case ' ' :
+		case '\t':
+		case '\r':
+		case '\n': return  1;
+		case '\0': return -1;
+
+		default: return 0;
+	}
+}
+
+void executeBuiltInCommand(struct command c){
+	int index = isBuildCommand(c) - 1;
+
+	switch(index){
+		case 0: exit(0);
+	}
+
+}
+
+struct command parseCommand(char *s){
+	// Array of args
+	char **args = malloc(sizeof(char*)*size);
+	// Index of next free arg
+	int k= 0;
+
+	// Index of next free char in buff
+	int b = 0;
+	char buff[size+1]
+	
+	int i;
+	for(i=0;;i++){
+		if(iswspace(s[i]){
+			continue;
+		}
+		else{
+			buff[b++] = s[i];
+			if(iswspace(s[i+1])){
+				buff[b] = '\0';
+				args[k] = malloc(strlen(buff)));
+				strcpy(args[k], buff);
+				k++;
+				strcpy(buff, "");
+				b = 0;
+			}
+		}
+	}
+	
+	struct command cmd;
+	cmd.cmd = args[0];
+	cmd.args = args;
+
+	return cmd;
+}
+
+int isBuildCommand(struct command c){
+	int i = 0;
+	for(;i<builtInCmdSize;i++)
+		if(!strcmp(c.cmd, builtInCmds[i]))
+			return i+1;
+
+	return 0;
+}
+
+int main(int argc, char **argv){
+	while(1){
+		int childPid;
+		char* cmdLine;
+		struct command cmd;
+
+		printPrompt();
+
+		// cmdLine = readCommandLine();
+		cmdLine = readline("");
+
+		cmd = parseCommand(cmdLine);
+
+		if(isBuildCommand(cmd)){
+			executeBuiltInCommand(cmd);
+		}
+		else{
+			childPid = fork();
+			if(childPid == 0){
+				executeCommand(cmd);
+			}
+			else{
+				if(isBackgrounfJob(cmd)){
+					// Record in lsb
+				}
+				else{
+					waitpid(childPid);
+				}
+			}
+		}
+	}
+}
